dc973f0a15499c9f3047ebc5bfc4f24345c8687c
Vaidotas <Vaidotas@Vaidas-PC>
2015-01-28 13:04:23 -0800

    fixed greeting message

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 	public static void main(String[] args0){
 		Test test = new Test();
 		
-		System.out.println("a greeting");
+		System.out.println("This is a nice greeting. (making the team leader proud)");
+		System.out.println("-------------------------------------------------------");
 		test.display();
 		
 		System.out.println("test 2");
