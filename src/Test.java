140de450aeb74d0770c2eb9f9c11435b036b8411
Vaidotas <Vaidotas@Vaidas-PC>
2015-01-28 13:08:53 -0800

    implemented display method.

--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,10 @@ public class Test {
 	int data[] = {0, 1, 5};
 	
 	public void display(){
-		System.out.println("TODO: display test data");
+		System.out.println("My data:");
 		
 		for(int dat : data){
-			System.out.println("");
+			System.out.println("Data value: " + dat);
 		}
 	}
 }
