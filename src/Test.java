3bec94b41d6cbe1c7481319bdda0d89c63435a51
Vaidotas <Vaidotas@Vaidas-PC>
2015-01-28 13:26:21 -0800

    Changed data, added a comment

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,13 @@
 
 public class Test {
-	int data[] = {0, 1, 5};
+	int data[] = {0, 1, 5, 8, 763276, -9};
 	
 	public void display(){
 		System.out.println("My test data:");
 		
+		// some comment
 		for(int dat : data){
+			
 			System.out.println("Data value: " + dat);
 		}
 	}
