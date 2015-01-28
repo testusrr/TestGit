85efc3096f2fc3aba4a702fd8d4c3624ae7fb3dc
Other Developer <junk@gmail.com>
2015-01-28 13:40:25 -0800

    added class comment summing up values and then displaying

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,19 @@
 
+/*
+ * class comment
+ */
 public class Test {
 	int data[] = {0, 1, 5};
 	
 	public void display(){
 		System.out.println("My test data:");
 		
+		int sum = 0;
 		for(int dat : data){
 			System.out.println("Data value: " + dat);
+			
+			sum += dat;
 		}
+		System.out.println("Sum is: " + sum);
 	}
 }
